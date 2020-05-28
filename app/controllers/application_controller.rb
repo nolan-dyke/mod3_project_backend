cc7a58f643d02b5805611bbef3d09dac6e6452cc
nolan-dyke <nolan.g.dyke@gmail.com>
2020-05-28 16:08:42 -0600

    created before action to authenticate pattern actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    def authenticate
+        token = request.headers["Authorization"].split(" ")[1]
+        secret = Rails.application.secrets.secret_key_base 
+        @user_id = JWT.decode(token, secret)[0]["user_id"]
+    end 
 end
--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,23 +1,23 @@
 class PatternsController < ApplicationController
+    before_action :authenticate, only: [:create, :index]
     def index
-        headers = request.headers["Authorization"]
+        # headers = request.headers["Authorization"]
         
-        if !headers 
-            render json: {message: "nice try, asshole"}
-        else 
-            token = headers.split(" ")[1]
-            secret = Rails.application.secrets.secret_key_base 
-            payload = JWT.decode(token, secret).first 
+        # if !headers 
+        #     render json: {message: "nice try, asshole"}
+        # else 
+        #     token = headers.split(" ")[1]
+        #     secret = Rails.application.secrets.secret_key_base 
+        #     payload = JWT.decode(token, secret).first 
 
-            render json: {message: "check out your pattern, #{payload['username']}"}
-        end 
+        #     render json: {message: "check out your pattern, #{payload['username']}"}
+        # end 
+
+        @patterns = Pattern.where(user_id: @user_id)
+        render json: @patterns
     end 
 
     def create
-        token = request.headers["Authorization"].split(" ")[1]
-        secret = Rails.application.secrets.secret_key_base 
-        @user_id = JWT.decode(token, secret)[0]["user_id"]
-
         @pattern = Pattern.create(
             user_id: @user_id,
             # type: params[:type],
